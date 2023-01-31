7bf211a86f5949c16ac041ae6725020a1483f7da
huling <huling@ysbang.cn>
2023-01-31 11:51:15 +0800

    feat: 搜索配置

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,6 +17,51 @@ export default defineConfig({
   },
 
   themeConfig: {
+    // https://zhuanlan.zhihu.com/p/568538285
+    algolia: {
+      appId: 'SOQWM2XHFS',
+      apiKey: '2bef230dc5c3d129504bee89eb47f05a',
+      indexName: 'hulingdocs-index-01',
+      placeholder: '请输入关键字',
+      translations: {
+        button: {
+          buttonText: '搜索文档',
+          buttonAriaLabel: '搜索文档'
+        },
+        modal: {
+          searchBox: {
+            resetButtonTitle: '清除查询条件',
+            resetButtonAriaLabel: '清除查询条件',
+            cancelButtonText: '取消',
+            cancelButtonAriaLabel: '取消'
+          },
+          startScreen: {
+            recentSearchesTitle: '搜索历史',
+            noRecentSearchesText: '没有搜索历史',
+            saveRecentSearchButtonTitle: '保存至搜索历史',
+            removeRecentSearchButtonTitle: '从搜索历史中移除',
+            favoriteSearchesTitle: '收藏',
+            removeFavoriteSearchButtonTitle: '从收藏中移除'
+          },
+          errorScreen: {
+            titleText: '无法获取结果',
+            helpText: '你可能需要检查你的网络连接'
+          },
+          noResultsScreen: {
+            noResultsText: '无法找到相关结果',
+            suggestedQueryText: '你可以尝试查询',
+            reportMissingResultsText: '你认为该查询应该有结果？',
+            reportMissingResultsLinkText: '点击反馈'
+          },
+          footer: {
+            selectText: '选择',
+            navigateText: '切换',
+            closeText: '关闭',
+            searchByText: '搜索提供者'
+          }
+        }
+      }
+    },
     nav: nav(),
 
     sidebar: {
